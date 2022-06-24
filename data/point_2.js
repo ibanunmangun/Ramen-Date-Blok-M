5b8d750a37ecec4a8fe3b483740ff1cd4900f7e8
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2022-06-24 21:36:23 +0800

    Add files via upload

---
+++ data/point_2.js
@@ -0,0 +1,11 @@
+var json_point_2 = {
+"type": "FeatureCollection",
+"name": "point_2",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "name": "Kashiwa", "desc": "Kashiwa, Jl. Melawai VIII, RT.3\/RW.1, Melawai, Kebayoran Baru, South Jakarta City, Jakarta 12160, Indonesia", "X": "106.798726", "Y": "-6.2447204" }, "geometry": { "type": "Point", "coordinates": [ 106.7987263, -6.2447204 ] } },
+{ "type": "Feature", "properties": { "name": "Little Tokyo Blok M", "desc": "Little Tokyo Blok M, Jl. Melawai 6 No.15a, RT.3\/RW.1, Melawai, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12160, Indonesia", "X": "106.798982", "Y": "-6.2443268" }, "geometry": { "type": "Point", "coordinates": [ 106.7989826, -6.2443268 ] } },
+{ "type": "Feature", "properties": { "name": "M Bloc Space", "desc": "M Bloc Space, Panglima Polim St No.37, RT.1\/RW.1, Melawai, Kebayoran Baru, South Jakarta City, Jakarta 12160, Indonesia", "X": "106.798694", "Y": "-6.2413576" }, "geometry": { "type": "Point", "coordinates": [ 106.7986948, -6.2413576 ] } },
+{ "type": "Feature", "properties": { "name": "Daitokyo Sakaba", "desc": "Daitokyo Sakaba, Jl. Melawai 9 No.26, RT.3\/RW.1, Melawai, Kec. Kby. Baru, Kota Jakarta Selatan, Daerah Khusus Ibukota Jakarta 12160, Indonesia", "X": "106.800251", "Y": "-6.2453457" }, "geometry": { "type": "Point", "coordinates": [ 106.8002515, -6.2453457 ] } }
+]
+}
